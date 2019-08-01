dd2cd47144a87d101d081f1253c22df61ebb9092
Timothy Graupmann <tim.graupmann@razer.com>
2019-07-31 19:16:29 -0700

    Add new plugin

---
+++ Plugins/HTML5SamplePlugin/Source/HTML5SamplePlugin/Private/HTML5SamplePluginBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "HTML5SamplePluginBPLibrary.h"
+#include "HTML5SamplePlugin.h"
+
+UHTML5SamplePluginBPLibrary::UHTML5SamplePluginBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UHTML5SamplePluginBPLibrary::HTML5SamplePluginSampleFunction(float Param)
+{
+	return -1;
+}
+
